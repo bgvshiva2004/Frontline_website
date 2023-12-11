6627c1db572dea4b1284ef78dd349dddb2e8d681
unknown <ishitapandey200423@gmail.com>
2023-12-11 14:27:45 +0530

    product data

---
+++ company/src/data/Products.js
@@ -0,0 +1,26 @@
+import pic1 from "../public/pic1.jpg"
+import pic2 from "../public/pic2.jpg"
+import pic3 from "../public/pic3.jpg"
+export const Products= [
+    {
+        id:1,
+        name:'LEGACY UPS',
+        description1:'ONLINE UPS WITH ISOLATION',
+        image_url:pic1,
+        description2:''
+    },
+    {
+        id:2,
+        name:'ONLINE HF UPS',
+        description1:'ONLINE SYNERGY HF UPS SYSTEMS',
+        image_url:pic2,
+        description2:''
+    },
+    {
+        id:3,
+        name:'FSS 3300 SERIES',
+        description1:'ONLINE FSS 3300 UPS SYSTEMS',
+        image_url:pic3,
+        description2:''
+     }
+]
